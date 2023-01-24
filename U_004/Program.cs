6e5f6804135c6941b7c6f53a3083f3e7874bdcec
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-24 22:13:03 +0300

    Add задача №4

--- U_004/Program.cs
+++ U_004/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Promt(string message) // Promt - быстро
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());//возвращаемое значение конверт32 из консоль редлайн
+}
+
+int n = Promt("Введите число N ");
+//Math.Pow(n,2);// фукция выведения степени в квадрате, если 2
+int i = 1; // индекс равен единице
+
+while(i <= n)
+{
+    double result = Math.Pow(i,2);
+    System.Console.WriteLine($"Квадрат числа {i} = " + result);
+    i++;
+}
